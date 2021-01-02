b987fd403d71a2d0f93e7232ea06d1ab23806688
mertkaanerdem <mmertkaanerdem@gmail.com>
2021-01-02 21:29:51 +0300

    almost done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,57 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+//import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {createStackNavigator} from '@react-navigation/stack';
 
-import {CategoryPage, MenuPage, RecipePage} from './pages';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import {CategoryPage, MenuPage, RecipePage, SavedFoodsPage} from './pages';
 
 const Stack = createStackNavigator();
+const Tab = createMaterialBottomTabNavigator();
+
+function RestaurantStack() {
+  return (
+    <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Screen name="Category" component={CategoryPage} />
+      <Stack.Screen name="Menu" component={MenuPage} />
+      <Stack.Screen name="Recipe" component={RecipePage} />
+    </Stack.Navigator>
+  );
+}
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Category" component={CategoryPage} />
-        <Stack.Screen name="Menu" component={MenuPage} />
-        <Stack.Screen name="Recipe" component={RecipePage} />
-      </Stack.Navigator>
+      <Tab.Navigator
+        activeColor="rgba(150, 200, 200, 0.2)"
+        initialRouteName="Restaurant">
+        <Tab.Screen
+          name="Saved"
+          component={SavedFoodsPage}
+          options={{
+            tabBarLabel: 'Saved Meals',
+            tabBarIcon: ({color}) => (
+              <MaterialCommunityIcons name="save" color={'#F4B700'} size={26} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Restaurant"
+          component={RestaurantStack}
+          options={{
+            tabBarLabel: 'Restaurants',
+            tabBarIcon: ({color}) => (
+              <MaterialCommunityIcons
+                name="food-drumstick"
+                color={color}
+                size={26}
+              />
+            ),
+          }}
+        />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
--- src/styles/pages_styles.js
+++ src/styles/pages_styles.js
@@ -4,20 +4,40 @@ const deviceSize = Dimensions.get('window');
 
 const category_page = StyleSheet.create({
   container: {
-    backgroundColor: '#e91e63',
+    backgroundColor: 'rgba(150, 200, 200, 0.2)',
     flex: 1,
-    margin: 10,
-    borderRadius: 10,
   },
 });
 
 const menu_page = StyleSheet.create({
   container: {
-    backgroundColor: '#e91e63',
+    backgroundColor: 'rgba(150, 200, 200, 0.2)',
     flex: 1,
-    margin: 10,
-    borderRadius: 10,
   },
 });
 
-export {category_page, menu_page};
+const recipe_page = StyleSheet.create({
+  container: {
+    backgroundColor: 'rgba(150, 200, 200, 0.2)',
+    flex: 1,
+  },
+});
+
+const saved_paged = StyleSheet.create({
+  container: {
+    backgroundColor: 'rgba(150, 200, 200, 0.2)',
+    flex: 1,
+    justifyContent: 'center',
+  },
+  button: {
+    backgroundColor: '#F4B700',
+    elevation: 5,
+    borderRadius: 8,
+    padding: 10,
+    margin: 15,
+    alignContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export {category_page, menu_page, recipe_page, saved_paged};
